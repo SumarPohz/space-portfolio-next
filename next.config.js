dc3f0de6e76a0e8f1b54f7dbc0045dc74cfef5a3
Sumarbha Pohsnem <sumarpohz@gmail.com>
2026-01-11 14:24:24 +0530

    Deploy static site

--- next.config.js
+++ next.config.js
@@ -1,45 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/space-portfolio" : "",
-  assetPrefix: isProd ? "/space-portfolio/" : "",
 };
 
 module.exports = nextConfig;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /** @type {import('next').NextConfig} */
-// const repoName = "sumarpohz.github.io"; // 👈 CHANGE if repo name is different
-
-// const nextConfig = {
-//   output: "export",
-
-//   basePath: `/${repoName}`,
-//   assetPrefix: `/${repoName}/`,
-
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// module.exports = nextConfig;
